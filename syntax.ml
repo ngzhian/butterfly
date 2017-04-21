994aacdbc2f818563f22f8d34442a20dbd4aeda2
Ng Zhi An <ngzhian@gmail.com>
2017-04-21 13:49:13 +0800

    Add toplevel for reading files

--- syntax.ml
+++ syntax.ml
@@ -18,26 +18,27 @@ type ty =
 (* dirt is the list of effects that may be called during evaluation *)
 and dirt = op list
 
-(* Dirty types are pure types with dirt, dirt is a list of effect types *)
+(* Dirty types are pure types with dirt, dirt is a list of effect names *)
 and dirty = ty * dirt
 
 (* Expressions are pure, evaluating them will not result in side effects *)
 type expr =
   | Unit
-  | Bool of bool                         (* Boolean constant *)
+  | Bool of bool                    (* Boolean constant *)
   | Fun of name * name * ty * comp  (* Function abstraction, fun f (x : bool) -> true *)
-  | Effect of name * ty * ty             (* Effect definition, effect choice : unit -> bool *)
-  | Var of name                          (* Variable *)
-  | Handler of clause * clause list      (* Handler, with clauses, handler | #op _ k -> true *)
+  | Effect of name * ty * ty        (* Effect definition, effect choice : unit -> bool *)
+  | Var of name                     (* Variable *)
+  | Handler of clause * clause list (* Handler, with clauses, handler | #op _ k -> true *)
+(* the first clause is the value clause which is always required *)
 
 and clause =                             (* clauses in a handler *)
-  | PVal of name * ty * comp             (* When a value is returned *)
+  | PVal of name * ty * comp             (* When a value is returned, val x : ty -> c *)
   | PEffect of name * name * name * comp (* | #op y k -> c, y is the argument #op is called with *)
 
 and comp =
   | Val of expr           (* Expression *)
   | Handle of comp * expr (* Handling a computation, handle (#op ()) with handler *)
-  | App of expr * comp    (* Function application *)
+  | App of expr * expr    (* Function application *)
   | Let of name * comp    (* Let expression *)
 
 (* Conversion from syntax to string *)
@@ -46,9 +47,9 @@ let rec string_of_expr (e : expr) : string =
   | Unit -> "()"
   | Bool b -> string_of_bool b
   | Fun (f, arg, tya, body) -> "fun " ^ f ^ "(" ^ arg ^ ")" ^ " : " ^ (string_of_comp body)
-  | Effect (e,ty1,ty2) -> "effect " ^ e
+  | Effect (e,ty1,ty2) -> "effect " ^ e ^ " : " ^ (string_of_tys ty1 ty2)
   | Var x -> x
-  | Handler (v, cls) -> String.concat " | " (List.map string_of_clause (v :: cls))
+  | Handler (v, cls) -> "handler | " ^ String.concat " | " (List.map string_of_clause (v :: cls))
 
 and string_of_clause (cls: clause) : string =
   match cls with
@@ -59,7 +60,7 @@ and string_of_comp (c: comp) : string =
   (match c with
    | Val e -> string_of_expr e
    | Handle (c,h) -> "handle " ^ (string_of_comp c) ^ " with " ^ (string_of_expr h)
-   | App (e1, c2) -> (string_of_expr e1) ^ " " ^ (string_of_comp c2)
+   | App (e1, c2) -> (string_of_expr e1) ^ " " ^ (string_of_expr c2)
    | Let (v, c) -> "let " ^ v ^ " = " ^ (string_of_comp c))
 
 and string_of_ty (ty : ty) =
@@ -67,9 +68,20 @@ and string_of_ty (ty : ty) =
   | TUnit  -> "TUnit"
   | TBool  -> "TBool"
   | TArrow (ty1, ty2) -> "TArrow(" ^ (string_of_ty ty1) ^ "," ^ (string_of_dirty ty2) ^ ")"
-  | TEffect (e, ty1, ty2) -> "TEffect(" ^ e ^ ", " ^ (string_of_ty ty1) ^ ", " ^ (string_of_ty ty2) ^ ")"
+  | TEffect (e, ty1, ty2) -> (string_of_ty ty1) ^ " -{" ^ e ^ "}> " ^ (string_of_ty ty2)
   | THandler (_,_) -> "THandler"
+
+and string_of_tys ty1 ty2 =
+  (string_of_ty ty1) ^ "->" ^ (string_of_ty ty2)
 
 and string_of_dirty (ty: dirty) =
   match ty with
   | (ty, dirt) -> string_of_ty ty ^ "[" ^ (String.concat ", " dirt) ^ "]"
+
+(* Top level used to read in files *)
+type toplevel = Expr of expr | Comp of comp
+
+let string_of_toplevel (tl: toplevel) =
+  match tl with
+  | Expr e -> string_of_expr e
+  | Comp c -> string_of_comp c
