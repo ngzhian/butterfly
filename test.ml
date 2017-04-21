852aea7c33963e9192b07901cade2b9fc9311831
Ng Zhi An <ngzhian@gmail.com>
2017-04-21 13:49:27 +0800

    Add tests to read from file typecheck and print

--- test.ml
+++ test.ml
@@ -1,21 +1,42 @@
+open Parser
 open Syntax
 open Type
 
-let op = "choice"
-let x = Bool true;;
-let eff = Effect (op, TUnit, TBool);;
-let eff_ty = TEffect (op, TUnit, TBool);;
-let op_call = App (Var op, Val Unit);;
-let handler = Handler (PVal ("x", TBool, Val (Var "x")), []);;
-let handle = Handle (op_call, handler);;
-let actually_handler = Handler (PVal ("x", TBool, Val (Var "x")), [PEffect (op, "y", "k", Val Unit)]);;
-let actually_handle = Handle (op_call, actually_handler);;
+(* Inititialize context by collecting all effects and lets *)
+let collect_context (ast : toplevel list) : context =
+  let rec go ast acc =
+    match ast with
+    | [] -> acc
+    | Expr (Effect (e, ty1, ty2) as expr) :: xs ->
+      go xs ((e, (type_expr expr acc, [])) :: acc)
+    | Comp (Let (v, c)) :: xs ->
+      go xs ((v, type_comp c []) :: acc)
+    | _::xs -> go xs acc
+  in
+  go ast []
+;;
+
+let type_check (effects : context) (toplevel : toplevel) =
+  let ty = match toplevel with
+    | Expr e -> type_expr e effects, []
+    | Comp c -> type_comp c effects
+  in
+  let _ = print_endline (string_of_dirty ty)
+  in ()
 
 let main () =
-  print_endline (string_of_expr x)
+  (* TODO warn if file name not given *)
+  let filename = Sys.argv.(1) in
+  let ch = open_in filename in
+  let lexbuf = Lexing.from_channel ch in
+  let ast = (Parser.file Lexer.token) lexbuf in
+  (* first collect effects, assume they are global *)
+  let effects = collect_context ast in
+  (* using the effects as context, type check the file *)
+  let _ = List.map (type_check effects) ast in
+  (* type check all expr/comp in file *)
+  (* when encounter an effect definition, add to context *)
+  print_endline (String.concat "\n" (List.map string_of_toplevel ast))
 ;;
-print_endline (string_of_expr x);;
-print_endline (string_of_comp handle);;
-print_endline (string_of_dirty (type_comp op_call [(op, eff_ty)]));;
-print_endline (string_of_dirty (type_comp handle [(op, eff_ty)]));;
-print_endline (string_of_dirty (type_comp actually_handle [(op, eff_ty)]));;
+
+main ()
